0dc081da1339144b1acc6f4f4b256eb187ad06ef
Gabriel Girouard <admin@bungeetaco.com>
2020-01-13 00:19:13 -0500

    Update 00002-custom-boot-animation.sh

--- 00002-custom-boot-animation.sh
+++ 00002-custom-boot-animation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BOOT_ANIMATION="https://github.com/corrmaan/rattlesnakeos-scripts/raw/master/bootanimation.zip"
+BOOT_ANIMATION="https://github.com/Bungeetaco/rattlesnakeos-scripts/raw/master/bootanimation.zip"
 mkdir -p $BUILD_DIR/branding
 echo "Downloading boot animation $BOOT_ANIMATION"
 wget -O $BUILD_DIR/branding/bootanimation.zip $BOOT_ANIMATION
-sed -i "\$aPRODUCT_COPY_FILES += branding/bootanimation.zip:system/media/bootanimation.zip" ${BUILD_DIR}/build/make/common/core.mk
+sed -i "\$aPRODUCT_COPY_FILES += branding/bootanimation.zip:system/media/bootanimation.zip" ${BUILD_DIR}/build/make/target/product/handheld_system.mk
